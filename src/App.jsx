145e42874d0aa6faa08b048a2d640693a17ab9af
Sreehari <sreeharisanjeev2004@gmail.com>
2024-06-23 18:42:06 +0530

    modified BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   }, []);
   return (
     
-    <BrowserRouter>
+    <BrowserRouter basename='/Portfolio/'>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
